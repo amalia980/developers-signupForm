e540d1bedd432fa7ed4f6a9c73cbb95be9ed4381
Amalia <74189129+chocolateprincess666@users.noreply.github.com>
2021-08-14 09:14:31 +0200

    testing to push, 2021-08-14

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 
 const myInputs = document.getElementsByClassName("inputs");
 const myBtn = document.getElementsById("submit-btn");
+const myFirstName = document.getElementById("firstname");
+const myForm = document.getElementById("form");
 
 myBtn.addEventListener("click", function(e) {
     e.preventDefault();
 
+    alert();
 
-    if (myInputs.length === "") {
+    /*
+    const validate = myFirstName.value;
+    const str = validate.length;
+
+    if (str == "") {
         alert("Please fill in every input");
     }
+    */
 });
 
